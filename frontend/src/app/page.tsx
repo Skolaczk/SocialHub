1cffd28d28fa5b1c4da40b6d28be0b3d303e7ccd
Michał <skolakmichal1@gmail.com>
2023-06-24 20:51:19 +0200

    Create basic theme

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -4,7 +4,7 @@ import { Posts } from '@/features';
 const Home = () => {
   return (
     <>
-      <h1 className="text-9xl">Hello world</h1>
+      <h1>Hello world</h1>
       <Button />
       <Posts />
     </>
