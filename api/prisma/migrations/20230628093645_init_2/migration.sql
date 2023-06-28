3ff3e9fc819935b0e146a794be9ea3b5a39999ac
Michał <skolakmichal1@gmail.com>
2023-06-28 11:58:44 +0200

    Configure api

---
+++ api/prisma/migrations/20230628093645_init_2/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "Posts" (
+    "id" SERIAL NOT NULL,
+    "content" TEXT NOT NULL,
+
+    CONSTRAINT "Posts_pkey" PRIMARY KEY ("id")
+);
---
+++ api/src/users/users.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class UsersService {
+  constructor(private prisma: PrismaService) {}
+
+  findAll() {
+    return this.prisma.user.findMany();
+  }
+
+  create() {
+    return this.prisma.user.create({ data: { name: 'Michał' } });
+  }
+}
