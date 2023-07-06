054d86a44ba894aaf7ace1bfc70fb13a9487aa07
Michał <76774237+Skolaczk@users.noreply.github.com>
2023-07-06 15:24:05 +0200

    Feature/SH-11-modify-users-GET-request (#21)

--- api/src/posts/posts.controller.ts
+++ api/src/posts/posts.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { PostsService } from 'src/posts/posts.service';
+import { Post } from '@prisma/client';
 
 @Controller('posts')
 export class PostsController {
   constructor(private postsService: PostsService) {}
 
   @Get()
-  findAll() {
+  findAll(): Promise<Post[]> {
     return this.postsService.findAll();
   }
 }
--- api/src/users/users.controller.ts
+++ api/src/users/users.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
+import { User } from '@prisma/client';
 
 @Controller('users')
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
   @Get()
-  findAll() {
-    return this.usersService.findAll();
+  findAllByUsername(@Query('username') username: string): Promise<User[]> {
+    return this.usersService.findAllByUsername(username);
   }
 }
