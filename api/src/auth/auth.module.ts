6a6edd02260754f375bc6b9467502536da3459d8
Michał <skolakmichal1@gmail.com>
2023-07-13 21:15:15 +0200

    Create custum guard and decorator

--- api/src/auth/auth.module.ts
+++ api/src/auth/auth.module.ts
@@ -3,11 +3,12 @@ import { PrismaModule } from 'src/prisma/prisma.module';
 import { AuthController } from 'src/auth/auth.controller';
 import { AuthService } from 'src/auth/auth.service';
 import { UsersModule } from 'src/users/users.module';
-import { JwtModule, JwtService } from '@nestjs/jwt';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from 'src/auth/strategy';
 
 @Module({
   imports: [PrismaModule, UsersModule, JwtModule.register({})],
   controllers: [AuthController],
-  providers: [AuthService, JwtService],
+  providers: [AuthService, JwtStrategy],
 })
 export class AuthModule {}
