163689fba5cd6b59ee6c9701c117c8e2bdaf34f5
Michał <skolakmichal1@gmail.com>
2023-07-13 20:22:18 +0200

    Create signIn functionality

--- api/src/auth/auth.service.ts
+++ api/src/auth/auth.service.ts
@@ -1,4 +1,8 @@
-import { ForbiddenException, Injectable } from '@nestjs/common';
+import {
+  ForbiddenException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { SignInDto, SignUpDto } from 'src/auth/dto';
 import { UsersService } from 'src/users/users.service';
@@ -11,25 +15,35 @@ export class AuthService {
     private usersService: UsersService,
   ) {}
 
-  signIn(signInDto: SignInDto) {
-    return this.usersService.findOneByEmail(signInDto.email);
+  async signIn(signInDto: SignInDto) {
+    const user = await this.usersService.findOneByEmail(signInDto.email);
+
+    if (!user) throw new UnauthorizedException('Credentials incorrect');
+
+    const isMatch = await bcrypt.compare(signInDto.password, user.password);
+
+    if (!isMatch) throw new UnauthorizedException('Credentials incorrect');
+
+    return user;
   }
 
-  async signUp({ email, username, password }: SignUpDto) {
-    const isEmailExists = await this.usersService.findOneByEmail(email);
+  async signUp(signUpDto: SignUpDto) {
+    const isEmailExists = await this.usersService.findOneByEmail(
+      signUpDto.email,
+    );
     const isUsernameExists = await this.usersService.findOneByUsername(
-      username,
+      signUpDto.username,
     );
 
     if (isEmailExists || isUsernameExists) {
       throw new ForbiddenException('Credentials taken');
     }
 
-    const hashPassword = await bcrypt.hash(password, 10);
+    const hashPassword = await bcrypt.hash(signUpDto.password, 10);
 
     return this.usersService.create({
-      email: email,
-      username: username,
+      email: signUpDto.email,
+      username: signUpDto.username,
       password: hashPassword,
     });
   }
