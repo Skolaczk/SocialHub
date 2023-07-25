3fc6d45b9b17077705178b083a7d592b79c3c3ac
Michał <skolakmichal1@gmail.com>
2023-07-25 21:09:03 +0200

    Split into smaller components

--- frontend/src/app/(general)/profile/[slug]/page.tsx
+++ frontend/src/app/(general)/profile/[slug]/page.tsx
@@ -1,7 +1,6 @@
 import { getUser } from '@/services';
 import { IParam } from '@/interfaces';
-import Image from 'next/image';
-import { PostsGrid } from '@/features';
+import { PostsGrid, UsersProfile } from '@/features';
 
 const Profile = async ({ params }: IParam) => {
   const user = await getUser(params.slug);
@@ -9,46 +8,7 @@ const Profile = async ({ params }: IParam) => {
   return (
     <div className="flex justify-center mb-14 md:mt-8 md:ml-20 xl:ml-0">
       <div className="max-w-2xl w-full">
-        <div className="p-5 md:pt-0">
-          <h1 className="text-center text-xl font-medium xs:hidden">
-            {user.username}
-          </h1>
-          <div className="flex items-center justify-between xs:justify-start xs:gap-8">
-            <Image
-              src={user.image}
-              alt=""
-              width={0}
-              height={0}
-              sizes="100vw"
-              className="rounded-full w-16 h-16 xs:w-32 xs:h-32"
-            />
-            <div>
-              <div className="hidden xs:flex gap-3">
-                <h1 className="text-xl font-medium">{user.username}</h1>
-                <button className="bg-primary rounded-sm py-1 px-5">
-                  Follow
-                </button>
-              </div>
-              <div className="flex gap-3 xs:my-5 xs:gap-4">
-                {Object.entries(user._count).map(([key, value]) => (
-                  <div
-                    key={key}
-                    className="flex flex-col items-center xs:flex-row-reverse"
-                  >
-                    <span className="font-bold">{value}</span>
-                    <span className="hidden xs:block mr-1">:</span>
-                    <span className="first-letter:uppercase">{key}</span>
-                  </div>
-                ))}
-              </div>
-              <p className="hidden xs:inline text-sm">{user.bio}</p>
-            </div>
-          </div>
-          <p className="text-sm mt-2 mb-5 xs:hidden">{user.bio}</p>
-          <button className="w-full bg-primary rounded-sm p-1 xs:hidden">
-            Follow
-          </button>
-        </div>
+        <UsersProfile user={user} />
         <PostsGrid posts={user.posts} />
       </div>
     </div>
