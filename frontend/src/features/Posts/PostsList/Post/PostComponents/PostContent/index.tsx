25424ea5eb09dddba18eef24050bb1835b7bbe1d
Michał <skolakmichal1@gmail.com>
2023-08-14 15:13:34 +0200

    Add links to posts

--- frontend/src/features/Posts/PostsList/Post/PostComponents/PostContent/index.tsx
+++ frontend/src/features/Posts/PostsList/Post/PostComponents/PostContent/index.tsx
@@ -1,19 +1,22 @@
 import Image from 'next/image';
 import { LikeIcon } from '@/assets/icons';
 import { IPost } from '@/interfaces';
+import Link from 'next/link';
 
-export const PostContent = ({ content, image, _count: count }: IPost) => {
+export const PostContent = ({ id, content, image, _count: count }: IPost) => {
   return (
     <>
       <p className="px-5 pb-5">{content}</p>
-      <Image
-        src={image}
-        alt=""
-        width={0}
-        height={0}
-        sizes="100vw"
-        className="w-full h-auto"
-      />
+      <Link href={`?post=${id}`}>
+        <Image
+          src={image}
+          alt=""
+          width={0}
+          height={0}
+          sizes="100vw"
+          className="w-full h-auto"
+        />
+      </Link>
       <div className="flex items-center justify-between p-5 text-sm">
         <div className="flex items-center gap-1">
           <button type="button">
@@ -21,7 +24,7 @@ export const PostContent = ({ content, image, _count: count }: IPost) => {
           </button>
           <p>{count.likes}</p>
         </div>
-        <p>{count.comments} comments</p>
+        <Link href={`?post=${id}`}>{count.comments} comments</Link>
       </div>
     </>
   );
