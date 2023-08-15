05ff151121bcc3154c6601a7f23c0ca340533dab
Michał <76774237+Skolaczk@users.noreply.github.com>
2023-08-15 11:30:23 +0200

    Bug/SH-47-fix-error-handling (#93)

--- frontend/src/app/(general)/profile/[slug]/page.tsx
+++ frontend/src/app/(general)/profile/[slug]/page.tsx
@@ -1,9 +1,14 @@
 import { getUser } from '@/services';
 import { IParam } from '@/interfaces';
 import { PostsGrid, UsersProfile } from '@/features';
+import { notFound } from 'next/navigation';
 
 const Profile = async ({ params }: IParam) => {
   const user = await getUser(params.slug);
+
+  if (!user) {
+    notFound();
+  }
 
   return (
     <div className="flex justify-center mb-14 md:mt-8 md:ml-20 xl:ml-0">
--- frontend/src/services/GetPost.service.ts
+++ frontend/src/services/GetPost.service.ts
@@ -2,6 +2,10 @@ import { api, endpoints } from '@/api';
 import { IPostWithComments } from '@/interfaces';
 
 export const getPost = async (id: number) => {
-  const { data } = await api.get<IPostWithComments>(`${endpoints.posts}/${id}`);
-  return data;
+  try {
+    const { data } = await api.get<IPostWithComments>(
+      `${endpoints.posts}/${id}`,
+    );
+    return data;
+  } catch (e) {}
 };
