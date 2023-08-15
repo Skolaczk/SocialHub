2179343ad27727c93185cf753d0962a76f1dde2f
Michał <skolakmichal1@gmail.com>
2023-08-15 21:22:09 +0200

    Move AddCommentForm to PostModal

--- frontend/src/features/Posts/PostModal/PostModalComponents/AddCommentForm/index.tsx
+++ frontend/src/features/Posts/PostModal/PostModalComponents/AddCommentForm/index.tsx
@@ -2,7 +2,7 @@ import { SendArrowIcon } from '@/assets/icons';
 
 export const AddCommentForm = () => {
   return (
-    <form className="flex items-center px-5 mb-5 gap-1">
+    <form className="flex items-center px-5 my-2 gap-1">
       <input
         className="bg-transparent w-full text-sm"
         type="text"
