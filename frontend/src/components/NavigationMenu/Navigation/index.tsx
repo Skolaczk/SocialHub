8c9180ccc28b493caefab76addca64929fe03f45
Michał <skolakmichal1@gmail.com>
2023-08-28 16:45:58 +0200

    Create array with navigation items

--- frontend/src/components/NavigationMenu/Navigation/index.tsx
+++ frontend/src/components/NavigationMenu/Navigation/index.tsx
@@ -11,37 +11,41 @@ interface IProps {
   readonly username: string;
 }
 
+const navigationItems = [
+  { href: '/', label: 'Home', icon: <HomeIcon />, order: 'order-1' },
+  {
+    href: '/explore',
+    label: 'Explore',
+    icon: <ExploreIcon />,
+    order: 'order-2',
+  },
+  {
+    href: '/notifications',
+    label: 'Notifications',
+    icon: <NotificationsIcon />,
+    order: 'order-4',
+  },
+  {
+    href: '/profile',
+    label: 'Profile',
+    icon: <ProfileIcon />,
+    order: 'order-5',
+  },
+];
+
 export const Navigation = ({ username }: IProps) => {
   return (
     <nav className="flex justify-between items-center w-full max-w-md px-5 py-3 md:flex-col md:w-auto md:max-w-auto md:w-fit md:gap-8 md:mt-8 xl:items-start xl:p-0 xl:mt-12">
-      <Link
-        href="/"
-        className="order-1 flex items-center gap-3 xl:w-full xl:py-1 xl:pr-20"
-      >
-        <HomeIcon />
-        <span className="hidden xl:block">Home</span>
-      </Link>
-      <Link
-        href="/explore"
-        className="order-2 flex items-center gap-3 xl:w-full xl:py-1 xl:pr-20"
-      >
-        <ExploreIcon />
-        <span className="hidden xl:block">Explore</span>
-      </Link>
-      <Link
-        href="/notifications"
-        className="order-4 flex items-center gap-3 xl:w-full xl:py-1 xl:pr-20"
-      >
-        <NotificationsIcon />
-        <span className="hidden xl:block">Notifications</span>
-      </Link>
-      <Link
-        href={`/profile/${username}`}
-        className="order-5 flex items-center gap-3 xl:w-full xl:py-1 xl:pr-20"
-      >
-        <ProfileIcon />
-        <span className="hidden xl:block">Profile</span>
-      </Link>
+      {navigationItems.map(({ href, label, icon, order }) => (
+        <Link
+          key={label}
+          href={href === '/profile' ? `/profile/${username}` : href}
+          className={`flex items-center gap-3 xl:w-full xl:py-1 xl:pr-20 ${order}`}
+        >
+          {icon}
+          <span className="hidden xl:block">{label}</span>
+        </Link>
+      ))}
       <Link
         href="/create"
         className="order-3 md:order-last xl:bg-primary xl:py-2 xl:rounded-sm xl:w-full xl:text-center"
