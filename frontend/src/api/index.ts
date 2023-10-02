4407457e0d64e9ec4c8f9d89710fe5e31388502e
Michał <skolakmichal1@gmail.com>
2023-10-02 19:51:33 +0200

    Create basic custom fetch

---
+++ frontend/src/api/index.ts
@@ -0,0 +1,24 @@
+import { baseURL, getToken } from './utils';
+
+export const api = {
+  get: async <T>(endpoint: string): Promise<T> => {
+    return await request('GET', endpoint);
+  },
+};
+
+const request = async <T>(method: string, endpoint: string): Promise<T> => {
+  const token = await getToken();
+
+  const requestOptions: RequestInit = {
+    method,
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+    next: {
+      tags: [endpoint],
+    },
+  };
+
+  const res = await fetch(`${baseURL}${endpoint}`, requestOptions);
+  return (await res.json()) as T;
+};
