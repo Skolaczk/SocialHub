44d30d5eaca98c564fe31a9cf93e8e653ffb6443
Michał <skolakmichal1@gmail.com>
2023-10-11 21:38:54 +0200

    Create GET endpoint

--- api/src/notifications/notifications.module.ts
+++ api/src/notifications/notifications.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { NotificationsService } from './notifications.service';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { NotificationsController } from 'src/notifications/notifications.controller';
 
 @Module({
   imports: [PrismaModule],
+  controllers: [NotificationsController],
   providers: [NotificationsService],
   exports: [NotificationsService],
 })
--- api/src/notifications/notifications.service.ts
+++ api/src/notifications/notifications.service.ts
@@ -1,10 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateNotification } from 'src/notifications/types';
+import { Notification } from '@prisma/client';
 
 @Injectable()
 export class NotificationsService {
   constructor(private prisma: PrismaService) {}
+
+  findAll(userId: number): Promise<Notification[]> {
+    return this.prisma.notification.findMany({ where: { userId } });
+  }
 
   create(data: CreateNotification) {
     if (data.userId === data.senderId) return;
