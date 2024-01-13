e3e113271c6715214e777182fbbe6f171c9fdd3a
Michał <skolakmichal1@gmail.com>
2024-01-13 16:03:26 +0100

    Fix errors

--- frontend/src/components/SocialPanel/index.tsx
+++ frontend/src/components/SocialPanel/index.tsx
@@ -4,12 +4,12 @@ import { UsersList } from './UsersList';
 import { getMe, getUsers } from '@/services';
 
 export const SocialPanel = async () => {
-  const { data } = await getMe();
+  const { data: user } = await getMe();
   const randomUsers = await getUsers();
 
   return (
     <div className="fixed right-0 top-0 hidden h-screen flex-col gap-8 border-l border-neutral-100 px-6 py-8 dark:border-neutral-900 xl:flex">
-      {data && <User user={data} isButton />}
+      {user && <User user={user} isButton />}
       <UsersList users={randomUsers} title="Your friends" />
     </div>
   );
